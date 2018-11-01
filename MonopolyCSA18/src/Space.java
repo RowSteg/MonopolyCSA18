bfc3371060f77c69febe3d610d48cd583120dfe4
53451 <53451@RMH171275.psdschools.org>
2018-11-01 12:31:05 -0600

    committed :)

--- MonopolyCSA18/src/Space.java
+++ MonopolyCSA18/src/Space.java
@@ -1,4 +1,24 @@
 
 public class Space {
-
+	
+	private final String name;
+	private final int xPos;
+	private final int yPos;
+	public Space(String name, int xPos, int yPos) {
+		this.name = name;
+		this.xPos = xPos;
+		this.yPos = yPos;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getXPos() {
+		return this.xPos;
+	}
+	
+	public int getYPos() {
+		return this.yPos;
+	}
 }
