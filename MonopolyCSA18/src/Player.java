d578539d4729bc16c099ad1b195cb9de3e3c70da
53457 <53457@RMH171276.psdschools.org>
2018-11-01 12:54:59 -0600

    Committed

---
+++ MonopolyCSA18/src/Player.java
@@ -0,0 +1,34 @@
+
+public class Player {
+	String name;
+	int xPos;
+	int yPos;
+	int amountOfMoney;
+	
+	public Player(String name) {
+		this.name = name;
+		xPos = 10;
+		yPos = 10;
+		amountOfMoney = 1500;
+	}
+	
+	public int[] getPos() {
+		return new int[] {xPos, yPos};
+	}
+	
+	public int getXPos() {
+		return xPos;
+	}
+	
+	public int getYPos() {
+		return yPos;
+	}
+	
+	public int getAmountOfMoney() {
+		return amountOfMoney;
+	}
+	
+	public String getName() {
+		return name;
+	}
+}
