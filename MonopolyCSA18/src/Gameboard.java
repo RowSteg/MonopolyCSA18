096ffb6b0939067b20108fd8fc89e2d052852d96
59262 <59262@RMH171281.psdschools.org>
2018-11-01 13:03:44 -0600

    Pencilled in gameboard pseudocode

---
+++ MonopolyCSA18/src/Gameboard.java
@@ -0,0 +1,19 @@
+
+public class Gameboard {
+	private Space[][] board;
+	
+	Gameboard(){
+		//board[0][0] = free parking
+		//board[10][0] = jail
+		//board[0][10] = go to jail
+		//board[10][10] = go
+		
+		//board[0][5] = railroad
+		//board[5][0] = railroad
+		//board[10][5] = railroad
+		//board[5][10] = railroad
+		
+		
+		
+	}
+}
