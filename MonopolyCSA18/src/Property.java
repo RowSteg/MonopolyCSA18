404c73375c8ee6a1eacfe2405f6f01c2aed086c1
Tyson <olear@10.0.0.14>
2018-11-03 15:16:23 -0600

    Added mortgage and house price.

--- MonopolyCSA18/src/Property.java
+++ MonopolyCSA18/src/Property.java
@@ -2,18 +2,26 @@ import java.util.HashMap;
 public class Property extends Space{
 
 	private final String color;
+	
 	private final int price;
+	private int mortgage;
+	private int housePrice;
+	
 	private int level; // level 0 - 5 representing number of houses/ hotel.
 	private HashMap<Integer, Integer> rents; // stores level as the key and rent price as the value
+	
 	private boolean owned;
 	private Player owner;
 	
-	public Property(String name, int xPos, int yPos, String color, int price, int... rents) { //rents should always be an array of length 6
+	
+	public Property(String name, int xPos, int yPos, String color, int price, int mortgage, int housePrice, int... rents) { //rents should always be an array of length 6
 		super(name, xPos, yPos);
 		this.color = color;
 		this.price = price;
+		this.mortgage = mortgage;
+		this.housePrice = housePrice;
 		this.level = 0;
-		owned  = false;
+		this.owned  = false;
 		for(int i = 0; i < 6; i++) {
 			this.rents.put(i,  rents[i]);
 		}
@@ -56,6 +64,13 @@ public class Property extends Space{
 		owned = true;
 		owner = p;
 	}
-
 	
+	//Mortgage
+	public int getMortgage() {
+		return mortgage;
+	}
+	
+	public int getHousePrice() {
+		return housePrice;
+	}
 }
